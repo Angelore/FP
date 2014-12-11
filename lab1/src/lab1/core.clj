31fc83e9665348c21bd920dec3e7aeaad1397757
Stanislau Blashko <angelore.main@gmail.com>
2014-12-11 11:49:57 +0300

    [lab1] Added alpha and beta calculation methods; added main method with a stub

--- lab1/src/lab1/core.clj
+++ lab1/src/lab1/core.clj
@@ -13,14 +13,33 @@
   (count (filter true? (map not= c1 c2))))
 
 ;; Constants
-(def Ra 3)            ; Ra, for defining a point potential
+(def Ra 3.0)            ; Ra, for defining a point potential
 (def Rb (* 1.5 Ra))   ; Rb, for reducing a point potential
 (def EpsilonTop 0.5)  ; EpsilonTop, the highest margin for estimation
 (def EpsilonBot 0.15) ; EpsilonBot, the lowest margin for estimation
+
+(defn alpha
+  "Calculate alpha param for calculating point potential." ;Change to const?
+  []
+  (/ 4 (* Ra Ra)))
+
+(defn beta
+  "Calculate beta param for calculating point potential."
+  []
+  (/ 4 (* Rb Rb)))
+
+(alpha)
+(beta)
 
 (defn parse-string
   "Returns a vector with numbers to process"
   [inputStr]
   (into [] (map read-string (drop-last (map string/trim (string/split inputStr #",")))))) ; Check performance in comparsion to butlast? Whatever
 
-(parse-string "5.1,3.5,1.4,0.2,Iris-setosa")
+(defn -main
+  [& args]
+  (if (< (count args) 2)
+    (println "Not enough arguments. Please go and stay go.")
+    (do)
+  )
+)
