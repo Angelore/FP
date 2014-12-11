c820cdb3ae2f959c1f69fc2c4e134749f1a51c97
Stanislau Blashko <angelore.main@gmail.com>
2014-12-11 11:01:45 +0300

    [lab1] Added distances and tests for them

--- lab1/src/lab1/core.clj
+++ lab1/src/lab1/core.clj
@@ -1,2 +1,14 @@
-(ns lab1.core)
+(ns lab1.core
+  (:require [clojure.string :as string])
+  )
 
+(defn euclidean-distance
+  [c1 c2]
+  (reduce + (map #(* % %) (map - c1 c2))))
+
+(defn hamming-distance
+  [c1 c2]
+  (count (filter true? (map not= c1 c2))))
+
+(hamming-distance [1 2 3] [1 3 5])
+(euclidean-distance [1 2 3] [1 3 5])
--- lab1/test/lab1/core_test.clj
+++ lab1/test/lab1/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [lab1.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest hamming-test
+  (testing "Hamming distance has been computed incorrectly."
+    (is (= 2 (hamming-distance [1 2 3] [1 3 5])))))
+
+(deftest euclidean-test
+  (testing "Euclidean distance has been computed incorrectly."
+    (is (= 5 (euclidean-distance [1 2 3] [1 3 5])))))
+
+(hamming-test)
+(euclidean-test)
+
+;5.1,3.5,1.4,0.2,Iris-setosa
