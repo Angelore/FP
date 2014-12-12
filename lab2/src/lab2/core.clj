cf87fe9d467eb17412149e676f885b398313c93b
Stanislau Blashko <angelore.main@gmail.com>
2014-12-12 10:30:19 +0300

    [lab2] Added code sections

--- lab2/src/lab2/core.clj
+++ lab2/src/lab2/core.clj
@@ -1,6 +1,28 @@
 (ns lab2.core)
 
+;; CRAAAAAAAAAAWLING IIIIIIIIIIIN MY SKIIIIIIIIIIIIN
+
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+
+;; Service functions
+
+;; End Service functions
+
+
+;; Parsing and whatnot
+
+;; End Parsing and whatnot
+
+
+;; Test launch!
+
+;; End Test launch!
+
+
+;; Entry point
+
+;; End Entry point
