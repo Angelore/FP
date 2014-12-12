109dff7b36181880c1679f960ef7c4bd6b842aa2
Stanislau Blashko <angelore.main@gmail.com>
2014-12-12 13:24:43 +0300

    [lab2] Added redirect handle, added url parsing function

--- lab2/src/lab2/core.clj
+++ lab2/src/lab2/core.clj
@@ -29,6 +29,20 @@
     (catch Object _ {:status 404 :headers nil})
    ))
 
+(defn is-redirect-status?
+  "Determines, if the status is equal to one of the 3xx statuses/"
+  [status]
+  (reduce #(or %1 %2) (map #(= status %) '(300 301 302 303 307))))
+
+(defn get-redirected-url
+  "Takes the contents of http request, and returns an url if the request was redirected. Otherwise, returns nil."
+  [content]
+  (let [status (:status content)]
+    (if (is-redirect-status? status)
+      (:location (:headers content))
+      nil)))
+
+
 (:headers (fetch-url "http://google.com/nopagehere"))
 (fetch-url "http://fkasdflkajsdf.com")
 (fetch-url "file:///E:/Projects/Clojure/FP-Clojure/lab2/swannodette_enlive-tutorial.htm")
@@ -40,6 +54,7 @@
   (contains? (set (map string/trim (string/split inputStr #";"))) "text/html"))
 
 (defn normalize-content
+  "Takes the contents of http request and returns enlive node list, if the content contains html page. Otherwise, returns nil."
   [content]
   (let [content-type (:content-type (:headers content))]
     (cond
@@ -53,27 +68,32 @@
   )
 )
 
+(defn get-urls
+  "Generates a list of links in envive node list." ; Example here: https://github.com/swannodette/enlive-tutorial/
+                                                   ; This function will also return links like "/otherpage.html".
+  [normalized-content]
+  (un-nil (map #(:href (:attrs %)) (html/select normalized-content #{[:a]}))))
+
+
 (normalize-content (fetch-url "http://google.com"))
+(get-urls (normalize-content (fetch-url "http://google.com")))
+(html/select (normalize-content (fetch-url "http://google.com")) #{[:a]})
 ;; End Service functions
 
 
-;; Parsing and whatnot
+;; Parsing files and whatnot
 (defn parse-blob
-  "Returns a vector with numbers to process"
+  "Returns a vector with strings to process."
   [inputStr]
-  (into [] (string/split-lines inputStr))) ; Check performance in comparsion to butlast? Whatever
+  (into [] (string/split-lines inputStr)))
 
 (defn parse-file
-  "Parses a source file and returns a list of vectors and yada yada yada"
+  "Parses a source file and returns a list of vectors and yada yada yada."
   [fileName]
   (parse-blob (slurp fileName)))
 
 (parse-file "addresses.txt")
-
-
-;(slurp "testhtml.html")
-;(html/select (html/html-resource (slurp "file:///E:/Projects/Clojure/FP-Clojure/lab2/swannodette_enlive-tutorial.htm")) #{[:a]})
-;; End Parsing and whatnot
+;; End Parsing files and whatnot
 
 
 ;; Test launch!
